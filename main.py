3ee65ad4b70b73af726871dc53f3e60552b858f7
Richard <richard@stromer.org>
2023-04-15 15:25:37 -0700

    get boilerplate working

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://chat.openai.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    host = "http://127.0.0.1:8000/"
+
+    with open("openapi.yaml") as f:
+        text = f.read()
+        # This is a trick we do to populate the PLUGIN_HOSTNAME constant in the OpenAPI spec
+        text = text.replace("PLUGIN_HOSTNAME", host)
+        return text
