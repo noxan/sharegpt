7972737b25488daf9959360e9f091c6499fcc9a8
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-04-15 18:20:53 -0700

    conversation ids

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import hashlib
 
 app = FastAPI()
 app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
@@ -21,18 +22,29 @@ class Item(BaseModel):
 memory = dict()
 
 @app.get("/load/<name>")
-def load(name: str):
-    return memory.get(name, "")
+def load(conv_id: str):
+    """
+    returns the conversation stored under the given hash, and generates a summary of it. Do not print out the conversation itself.
+    """
+    return memory.get(conv_id, "")
 
 
 @app.post("/save")
-async def save(name: str, item: Item):
-    memory[name] = item.content
-    print("save", item)
-    return {"status": "ok"}
+async def save(conversation: Item):
+    """
+    stores the conversation, excluding ShareGPT `save` and `load` commands and ChatGPT's responses with a generated hash as the key
+    returns the generated hash in the field 'id'
+    """
+    h = hashlib.new('sha256')
+    h.update(bytes(conversation.content, 'utf-8'))
+    conv_id = h.hexdigest()
+    
+    memory[conv_id] = conversation.content
+    print(conv_id, conversation.content)
+    return {"id": conv_id}
 
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", port=8000, reload=True)
