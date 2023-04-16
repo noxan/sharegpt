1bbfc9907b052824f5645c2ed6647b2880a89592
Richard <richard@stromer.org>
2023-04-16 09:51:14 -0700

    format code with black

--- main.py
+++ main.py
@@ -25,8 +25,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Item(BaseModel):
     content: str
+
 
 class Formula(BaseModel):
     name: str
@@ -43,21 +45,25 @@ def build_prompt_formula_prompt(formula_name, formula_description, formula_param
 def root():
     return {"status": "ok"}
 
+
 @app.get("/.well-known/ai-plugin.json", include_in_schema=False)
 def plugin():
     with open(".well-known/ai-plugin.json") as f:
         manifest = f.read()
     return Response(content=manifest, media_type="application/json")
 
+
 @app.get("/load/id/{conv_id}")
 def load(conv_id: str):
     item = memory.find_one({"_id": conv_id})
     return item.get("payload", "") if item else ""
 
+
 @app.get("/load/name/{name}")
 def load(name: str):
     item = memory.find_one({"payload.name": name})
     return item.get("payload", "") if item else ""
+
 
 @app.post("/save")
 async def save(json_payload: Dict):
@@ -72,9 +78,8 @@ async def save(json_payload: Dict):
     memory.insert_one({"_id": payload_id, "payload": json_payload})
     return {"id": payload_id}
 
-    
 
-@app.post('/save_formula')
+@app.post("/save_formula")
 async def save_formula(formula: Formula):
     """
     saves a formula, which is defined with fields name, description, and parameters
@@ -82,22 +87,26 @@ async def save_formula(formula: Formula):
     A formula is not code. The prompt name should contain no spaces.
     The returned id can be used to access the formula
     """
-    formula_name, fromula_description, formula_params = formula.name, formula.description, formula.parameters
-    h = hashlib.new('sha256')
-    h.update(bytes(formula_name + str(fromula_description) + str(formula_params), 'utf-8'))
+    formula_name, fromula_description, formula_params = (
+        formula.name,
+        formula.description,
+        formula.parameters,
+    )
+    h = hashlib.new("sha256")
+    h.update(
+        bytes(formula_name + str(fromula_description) + str(formula_params), "utf-8")
+    )
     prompt_id = h.hexdigest()
 
-    formula_prompt = build_prompt_formula_prompt(formula_name, fromula_description, formula_params)
-    memory.insert_one(
-        {
-            "_id": prompt_id,
-            "payload": formula_prompt
-        }
+    formula_prompt = build_prompt_formula_prompt(
+        formula_name, fromula_description, formula_params
     )
+    memory.insert_one({"_id": prompt_id, "payload": formula_prompt})
 
-    return {"id": prompt_id, 'formula_prompt': formula_prompt}
+    return {"id": prompt_id, "formula_prompt": formula_prompt}
 
-@app.get('/load_formula')
+
+@app.get("/load_formula")
 async def load_formula(formula_id: str):
     """
     returns the formula corresponding to the given hash (formula_id) from ShareGPT
@@ -110,4 +119,5 @@ app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
