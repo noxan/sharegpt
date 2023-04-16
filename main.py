97418614252cd5b5ccd63f188e8b3cd76b054a20
P/IO <pio@PIOs-MacBook-Air.local>
2023-04-15 22:53:46 -0700

    Add persistent memory in mongo

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import Response
 from pydantic import BaseModel
 import hashlib
+from typing import Dict
+from pymongo import MongoClient
 
-PRODUCTION_URL = "https://sharegpt-psi.vercel.app"
-DEVELOPMENT_URL = "http://localhost:8000"
-IS_VERCEL = bool(os.environ.get("VERCEL"))
-URL = PRODUCTION_URL if IS_VERCEL else DEVELOPMENT_URL
+MONGO_URI = "mongodb+srv://cerebral_valley:cerebral_valley_041523@cluster0.byzwutn.mongodb.net/?retryWrites=true&w=majority"
 
+client = MongoClient(MONGO_URI)
+db = client["sharegpt"]
+memory = db["memory"]
 
 app = FastAPI()
 
@@ -22,7 +24,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 class Item(BaseModel):
     content: str
 
@@ -31,80 +32,68 @@ class PromptFormula(BaseModel):
     body: str
     parameters: list
 
-prompt_memory = dict()
-conversation_memory = dict()
-
-
 @app.get("/", include_in_schema=False)
 def root():
     return {"status": "ok"}
-
 
 @app.get("/.well-known/ai-plugin.json", include_in_schema=False)
 def plugin():
     with open(".well-known/ai-plugin.json") as f:
         manifest = f.read()
-    content = manifest.replace("{{URL}}", URL)
-    return Response(content=content, media_type="application/json")
+    return Response(content=manifest, media_type="application/json")
 
-
-@app.get("/load/<name>")
+@app.get("/load/id/{conv_id}")
 def load(conv_id: str):
-    """
-    returns the conversation stored under the given hash, and generates a summary of it. Do not print out the conversation itself.
-    """
-    return conversation_memory.get(conv_id, "")
+    item = memory.find_one({"_id": conv_id})
+    return item.get("payload", "") if item else ""
 
+@app.get("/load/name/{name}")
+def load(name: str):
+    item = memory.find_one({"payload.name": name})
+    return item.get("payload", "") if item else ""
 
 @app.post("/save")
-async def save(conversation: Item):
-    """
-    stores the conversation, excluding ShareGPT `save` and `load` commands and ChatGPT's responses with a generated hash as the key
-    returns the generated hash in the field 'id'
-    """
+async def save(json_payload: Dict):
+    json_str = str(json_payload)
     h = hashlib.new("sha256")
-    h.update(bytes(conversation.content, "utf-8"))
-    conv_id = h.hexdigest()
-    
-    conversation_memory[conv_id] = conversation.content
-    return {"id": conv_id}
+    h.update(bytes(json_str, "utf-8"))
+    payload_id = h.hexdigest()
 
-@app.post('/save_prompt')
+    memory.insert_one({"_id": payload_id, "payload": json_payload})
+    return {"id": payload_id}
+
+@app.post("/save_prompt")
 async def save_prompt_formula(prompt_formula: PromptFormula):
-    """
-    saves a prompt formula, which is defined with fields name, body, and parameters
-    generates a hash for the formula which can be used to load it later
-    """
-    
     prompt_name, prompt_body, prompt_parameters = prompt_formula.name, prompt_formula.body, prompt_formula.parameters
-    h = hashlib.new('sha256')
-    h.update(bytes(prompt_name + str(prompt_body) + str(prompt_parameters), 'utf-8'))
+    h = hashlib.new("sha256")
+    h.update(bytes(prompt_name + str(prompt_body) + str(prompt_parameters), "utf-8"))
     prompt_id = h.hexdigest()
 
-    prompt_memory[prompt_id] = {
-        'name': prompt_name,
-        'prompt': prompt_body,
-        'parameters': prompt_parameters
-    }
+    memory.insert_one(
+        {
+            "_id": prompt_id,
+            "payload": {
+                "name": prompt_name,
+                "prompt": prompt_body,
+                "parameters": prompt_parameters,
+            }
+        }
+    )
 
     return {"id": prompt_id}
 
-@app.get('/load_prompt')
+@app.get("/load_prompt/{prompt_id}")
 async def load_prompt(prompt_id: str):
-    """
-    returns the prompt stored under the given hash (prompt_id)
-    interpret the text that is returned and do what it says
-    """
-    prompt_details = prompt_memory.get(prompt_id, "")
+    prompt_details = memory.find_one({"_id": prompt_id})
     if prompt_details:
-        prompt = f"When the user gives the command `ShareGPT run {prompt_details['name']} {prompt_details['parameters']}`, run the following prompt:\n\n{prompt_details['prompt']}\n\n \
+        payload = prompt_details["payload"]
+        prompt = f"When the user gives the command `ShareGPT run {payload['name']} {payload['parameters']}`, run the following prompt:\n\n{payload['prompt']}\n\n \
                     but replace anything in square brackets with the users input. The user will specify parameters in the correct order."
         return prompt
 
-app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
 
+app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
