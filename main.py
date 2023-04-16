c2f4c31c1bb37850c632a97c865580b028ba31e4
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-04-15 20:55:17 -0700

    prompt formulas kinda work

--- main.py
+++ main.py
@@ -24,8 +24,13 @@ app.add_middleware(
 class Item(BaseModel):
     content: str
 
+class PromptFormula(BaseModel):
+    name: str
+    body: str
+    parameters: list
 
-memory = dict()
+prompt_memory = dict()
+conversation_memory = dict()
 
 
 @app.get("/")
@@ -46,7 +51,7 @@ def load(conv_id: str):
     """
     returns the conversation stored under the given hash, and generates a summary of it. Do not print out the conversation itself.
     """
-    return memory.get(conv_id, "")
+    return conversation_memory.get(conv_id, "")
 
 
 @app.post("/save")
@@ -59,10 +64,40 @@ async def save(conversation: Item):
     h.update(bytes(conversation.content, 'utf-8'))
     conv_id = h.hexdigest()
     
-    memory[conv_id] = conversation.content
-    print(conv_id, conversation.content)
+    conversation_memory[conv_id] = conversation.content
     return {"id": conv_id}
 
+@app.post('/save_prompt')
+async def save_prompt_formula(prompt_formula: PromptFormula):
+    """
+    saves a prompt formula, which is defined with fields name, body, and parameters
+    generates a hash for the formula which can be used to load it later
+    """
+    
+    prompt_name, prompt_body, prompt_parameters = prompt_formula.name, prompt_formula.body, prompt_formula.parameters
+    h = hashlib.new('sha256')
+    h.update(bytes(prompt_name + str(prompt_body) + str(prompt_parameters), 'utf-8'))
+    prompt_id = h.hexdigest()
+
+    prompt_memory[prompt_id] = {
+        'name': prompt_name,
+        'prompt': prompt_body,
+        'parameters': prompt_parameters
+    }
+
+    return {"id": prompt_id}
+
+@app.get('/load_prompt')
+async def load_prompt(prompt_id: str):
+    """
+    returns the prompt stored under the given hash (prompt_id)
+    interpret the text that is returned and do what it says
+    """
+    prompt_details = prompt_memory.get(prompt_id, "")
+    if prompt_details:
+        prompt = f"When the user gives the command `ShareGPT run {prompt_details['name']} {prompt_details['parameters']}`, run the following prompt:\n\n{prompt_details['prompt']}\n\n \
+                    but replace anything in square brackets with the users input. The user will specify parameters in the correct order."
+        return prompt
 
 app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
 
