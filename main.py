99902ed21804bd964eb6c1ecf6f889656ed6c556
Richard <richard@stromer.org>
2023-04-15 17:23:49 -0700

    run main script with uvicorn

--- main.py
+++ main.py
@@ -30,3 +30,9 @@ async def save(name: str, item: Item):
     memory[name] = item.content
     print("save", item)
     return {"status": "ok"}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
